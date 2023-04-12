84a0adc2d8c6406e72fa2834b4a54d62ea65ef24
TheGreatHB <hb@thegreathb.com>
2023-04-12 22:53:35 +0900

    Support exponentials and exponentiation operators ^ and **

--- src/popup.js
+++ src/popup.js
@@ -32,24 +32,27 @@ function calculate() {
 }
 
 function evaluateExpression(expression) {
+  expression = expression.replace(/\*\*/g, "^"); // Replace ** with ^
+
   const operatorPrecedence = {
     "+": 1,
     "-": 1,
     "*": 2,
     "/": 2,
+    "^": 3,
   };
 
-  const tokens = expression.match(/\d+|[+\-*/()]/g);
+  const tokens = expression.match(/\d+(?:\.\d+)?(?:e[+-]?\d+)?|[+\-*/()^]/gi);
   const outputQueue = [];
   const operatorStack = [];
 
   for (const token of tokens) {
-    if (/\d+/.test(token)) {
-      outputQueue.push(token);
-    } else if (/[+\-*/]/.test(token)) {
+    if (/\d+(?:\.\d+)?(?:e[+-]?\d+)?/i.test(token)) {
+      outputQueue.push(parseExponentialToBigInt(token));
+    } else if (/[+\-*/^]/.test(token)) {
       while (
         operatorStack.length > 0 &&
-        /[+\-*/]/.test(operatorStack[operatorStack.length - 1]) &&
+        /[+\-*/^]/.test(operatorStack[operatorStack.length - 1]) &&
         operatorPrecedence[token] <=
           operatorPrecedence[operatorStack[operatorStack.length - 1]]
       ) {
@@ -80,9 +83,9 @@ function evaluateExpression(expression) {
   const evaluationStack = [];
 
   for (const token of outputQueue) {
-    if (/\d+/.test(token)) {
+    if (/\d+(?:\.\d+)?(?:e[+-]?\d+)?/i.test(token)) {
       evaluationStack.push(BigInt(token));
-    } else if (/[+\-*/]/.test(token)) {
+    } else if (/[+\-*/^]/.test(token)) {
       const b = evaluationStack.pop();
       const a = evaluationStack.pop();
 
@@ -99,6 +102,9 @@ function evaluateExpression(expression) {
         case "/":
           evaluationStack.push(a / b);
           break;
+        case "^":
+          evaluationStack.push(a ** b);
+          break;
       }
     }
   }
@@ -107,5 +113,22 @@ function evaluateExpression(expression) {
     return evaluationStack[0].toString();
   } else {
     throw new Error("Invalid expression");
+  }
+}
+
+function parseExponentialToBigInt(numString) {
+  const [significand, exponent] = numString.toLowerCase().split("e");
+  let [integerPart, decimalPart = ""] = significand.split(".");
+  const exp = parseInt(exponent, 10);
+
+  if (exp >= 0) {
+    return BigInt(
+      integerPart + decimalPart + "0".repeat(exp - decimalPart.length)
+    );
+  } else {
+    const decimalDigits = -exp - 1;
+    const shiftedIntegerPart = integerPart.slice(0, -decimalDigits) || "0";
+    const shiftedDecimalPart = integerPart.slice(-decimalDigits) + decimalPart;
+    return BigInt(shiftedIntegerPart + shiftedDecimalPart);
   }
 }
